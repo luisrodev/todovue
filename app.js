8dc05b8f007597385038fcc503d904a014c5241b
Luis A. Rubio <luirting@gmail.com>
2018-09-24 00:05:45 -0700

    Add todo in to the event list

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('Hello World')
+const app = new Vue({
+    el: '#todo',
+    data: {
+        event: {
+            id: '',
+            name: '',
+            description: '',
+            date: '2018-09-23'
+        },
+        events: []
+    },
+    mounted: function () {
+        console.log('Aplication running');
+    },
+    methods: {        
+        addEvent: function(){
+            if(this.event.name){
+                this.events.push(this.event);
+                this.event = {name: '', description: '', date: '2018-09-23'};
+            }
+        }
+    }
+});
